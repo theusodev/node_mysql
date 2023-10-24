d9f03bec39435fa7a974eaa61e1f7bf83df3e3a7
EA1 - Matheus Oliveira Da Silva <matheus_oliveira02@educadventista.org>
2023-10-24 20:41:06 -0300

    segundo commit adding data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
@@ -15,8 +16,30 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
-app.get("/",(requisicao, resposta) => {
-    resposta.render("home")
+app.post("/register/save", (request, response) => {
+    const { title, pageqty } = request.body
+
+    const query = `
+        INSERT INTO books (title, pageqty)
+        VALUES ('${title}', '${pageqty}')
+    `
+
+    conn.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.redirect("/")
+    })
+})
+
+app.get("/register", (request, response) => {
+    response.render("register")
+})
+
+app.get("/",(request, response) => {
+    response.render("home")
 })
 
 const conn = mysql.createConnection({
